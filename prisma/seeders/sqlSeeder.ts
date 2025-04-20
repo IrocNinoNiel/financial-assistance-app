c6243f6ee99d8397c110f93aab7355d9d6cbb9e8
IrocNinoNiel <iroc.niel123@gmail.com>
2025-04-20 13:02:22 +0800

    [FEAT] add script to run seeder

---
+++ prisma/seeders/sqlSeeder.ts
@@ -0,0 +1,34 @@
+import { PrismaClient } from '@prisma/client';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const prisma = new PrismaClient();
+
+const sqlFiles = [
+    { label: 'Region', file: '../locations/regions.sql' },
+    { label: 'Province', file: '../locations/provinces.sql' },
+    { label: 'City mun', file: '../locations/citymuns.sql' },
+    { label: 'Barangay', file: '../locations/barangays.sql' },
+    { label: 'School', file: '../locations/schools.sql' },
+];
+
+async function executeSqlFile(label: string, relativePath: string) {
+    const fullPath = path.join(__dirname, relativePath);
+    const sql = fs.readFileSync(fullPath, 'utf8');
+    await prisma.$executeRawUnsafe(sql);
+    console.log(`✅ SQL ${label} file executed successfully.`);
+}
+
+async function seedFromSqlFile() {
+    try {
+        for (const { label, file } of sqlFiles) {
+        await executeSqlFile(label, file);
+        }
+    } catch (error) {
+        console.error('❌ Failed to execute SQL file:', error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+
+seedFromSqlFile();
