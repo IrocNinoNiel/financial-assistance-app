9679ffbe2a866c2f74f3aea7a33c8ce8a9107665
IrocNinoNiel <iroc.niel123@gmail.com>
2025-05-07 22:39:16 +0800

    [FILE] add static path for docs

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express"
 import routes from "./routes";
 import * as dotenv from "dotenv";
+import path from "path";
 
 dotenv.config();
 const app = express()
@@ -11,6 +12,7 @@ const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/v1', routes);
 
 
